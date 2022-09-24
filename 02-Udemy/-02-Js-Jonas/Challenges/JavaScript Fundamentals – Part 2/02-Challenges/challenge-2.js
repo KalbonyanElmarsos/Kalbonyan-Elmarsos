64ccebaf511823e37870adb9a8a26c8b7f0f9e82
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-24 18:15:29 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 2/02-Challenges/challenge-2.js
@@ -0,0 +1,8 @@
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, total);
