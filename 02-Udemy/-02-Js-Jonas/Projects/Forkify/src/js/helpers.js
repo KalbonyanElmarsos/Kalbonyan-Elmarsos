77272fc92019254a233b7ceface0bcbb43be44c1
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-24 17:16:31 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Projects/Forkify/src/js/helpers.js
@@ -0,0 +1,32 @@
+import { async } from 'regenerator-runtime';
+import { TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
