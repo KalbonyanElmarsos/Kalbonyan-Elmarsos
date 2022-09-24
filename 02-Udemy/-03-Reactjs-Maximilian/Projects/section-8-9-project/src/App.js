023fa0b933fe88f453148c9fa0d1249d3abc5cc7
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-24 19:32:34 +0200

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/section-8-9-project/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import AddUser from "./Component/User/AddUser";
+import UserList from "./Component/User/UserList";
+
+function App() {
+  const [usersList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/section-8-9-project/src/Component/User/UserList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import classes from "./UserList.module.css";
+
+const UserList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
