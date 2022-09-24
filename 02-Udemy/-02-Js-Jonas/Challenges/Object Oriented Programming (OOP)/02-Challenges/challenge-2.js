9bf5d1fe52acd8a998a12261fcb22022c034b1ec
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-24 18:17:16 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/Object Oriented Programming (OOP)/02-Challenges/challenge-2.js
@@ -0,0 +1,32 @@
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = this.speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
