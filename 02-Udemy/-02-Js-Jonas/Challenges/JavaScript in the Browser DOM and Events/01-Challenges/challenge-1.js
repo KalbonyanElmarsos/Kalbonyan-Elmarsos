8ee367b10f4770ce6672b251870242da13459acb
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-24 18:29:32 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript in the Browser DOM and Events/01-Challenges/challenge-1.js
@@ -0,0 +1,18 @@
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
+});
