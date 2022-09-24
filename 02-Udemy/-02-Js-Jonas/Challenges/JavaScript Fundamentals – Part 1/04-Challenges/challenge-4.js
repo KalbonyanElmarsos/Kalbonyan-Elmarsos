d1f987229d760f41112da66c421ff17bb9cb02d5
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-24 18:14:03 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 1/04-Challenges/challenge-4.js
@@ -0,0 +1,8 @@
+// const bill = 275;
+// const bill = 40;
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
