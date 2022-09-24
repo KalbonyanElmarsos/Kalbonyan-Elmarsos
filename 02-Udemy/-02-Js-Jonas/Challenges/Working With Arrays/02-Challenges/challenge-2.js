1c4efe62ef9d95017725b054cdc37c6d8550c813
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-24 18:20:27 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/Working With Arrays/02-Challenges/challenge-2.js
@@ -0,0 +1,19 @@
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+
+  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+
+  // 2 3. (2+3)/2 === 2.5. 2/2+3/2 = 2.5
+
+  return average;
+};
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
