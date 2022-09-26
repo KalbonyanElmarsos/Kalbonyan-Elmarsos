1769bb75dc323bf91bd05f07eb31c0570ff0be8d
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-26 14:42:29 +0200

    Update Debugging-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
@@ -1,1 +1,19 @@
 
+start = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+print(start[3])
+
+peaks = {
+    'African': 'Kilimanjaro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_American': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconocagua',
+}
+print(peaks['Pacific'])
