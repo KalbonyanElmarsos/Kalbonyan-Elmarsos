2a2624442b60f84ab75c8f36f8fd9bc50b9a5da2
Menna Hassan <114239399+Menn26@users.noreply.github.com>
2022-09-26 14:32:25 +0200

    Add files via upload

---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/challenge-1.py
@@ -0,0 +1,14 @@
+name = input("what's your favorite food?")
+
+if name == "salad":
+    print("yep! So amazing")
+else:
+    print("yuck! That's not it")
+
+print("Thanks for playing")
+
+num = 36
+if num % 3 == 0:
+    print("is divisible by 3")
+else:
+    print("is not divisible by 3")
