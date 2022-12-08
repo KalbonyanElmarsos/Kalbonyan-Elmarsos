7ca41720fe0af648e3d4d9b2f8dd3caac9b445df
abdoali23 <83145855+abdoali23@users.noreply.github.com>
2022-12-08 02:48:24 +0200

    Add files via upload

---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Challenga _2.py
@@ -0,0 +1,25 @@
+print("Challengae _2:")
+
+# Gussing Game
+ques = input("What's my favorite food? ")            # asking user about my favorite  food
+
+# group of answers
+ans1 = "pasta"
+ans2 = "steak"
+ans3 = "chicken"
+
+# check the ans to ques
+if ques == ans1 : 
+  print("Yep! So amazing!")
+
+elif ques == ans2 :
+  print("Yep! So amazing!")
+
+elif ques == ans3 :
+  print("Yep! So amazing!")
+
+else : 
+  print("Yuck! That's not it!")
+
+print("Thanks for playing!")
+print("Challengae complete!")
